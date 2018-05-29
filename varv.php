9e087cc4d6c9a6bd57c26285b364b651a23a0c22
kadik <kadi.kerner@khk.ee>
2018-05-29 14:52:04 +0300

    Loodud värvilise teksti konstruktur

---
+++ varv.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anna.karutina
+ * Date: 14.12.2017
+ * Time: 13:35
+ */
+header('Refresh: 1');
+for($rida = 1; $rida <= 5; $rida++){
+    $varv = '#';
+    for($kord = 1; $kord <= 6; $kord++){
+        $juhuarv = rand(0, 15);
+        $symbol = dechex($juhuarv);
+        $varv = $varv.$symbol;
+    }
+    echo '<font color="'.$varv.'">'.
+        'Värviline tekst'.
+        '</font>'.
+        '<br />';
+}
