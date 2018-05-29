9cef1af9bb856bca36c30e89e12cb9a3f0789b0d
kadik <kadi.kerner@khk.ee>
2018-05-29 14:49:01 +0300

    Loodud värvilise teksti konstruktur

---
+++ vtekst.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Punane
+ * Date: 25.01.2018
+ * Time: 11:10
+ */
+require_once 'tekst.php';
+class vtekst extends tekst
+{
+    //lisame varvi ka
+    var $varv = '';
+
+    /**
+     * vtekst constructor.
+     * @param string $varv
+     */
+    public function __construct($sone, $varv)
+    {
+        parent::__construct($sone);
+        $this->maaraVarv($varv);
+    }
+
+    function maaraVarv($varv){
+        $this->varv = $varv;
+    }
+
+    function prindiTekst()
+    {
+        if ($this->varv == ''){
+            parent::prindiTekst();
+        } else {
+            echo '<font color="'.$this->varv.'">'.$this->sone.'</font><br />';
+        }
+
+    }
+}
